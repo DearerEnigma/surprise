3bab7977f71e4cbb12fafba2027496f6ea23047d
DearerEnigma <96772774+DearerEnigma@users.noreply.github.com>
2022-01-22 19:05:53 +0100

    Create enigme.js

---
+++ enigme.js
@@ -0,0 +1,8 @@
+alert("Ceci est un exemple de commande  JavaScript");
+text = prompt("Vous  aimez les tacos ?");
+if (text == "oui") {
+	alert("T'es un bon toi ;p")
+	}
+else {
+	alert("Achete toi une vie")
+	}
